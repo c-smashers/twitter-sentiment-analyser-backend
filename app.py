c2d89a05d57ed6381ebbe8012bb44ca7a663b15f
hemant1101 <hemantmandal1101@gmail.com>
2023-04-22 10:13:47 +0530

    added tokenizer and model

---
+++ app.py
@@ -0,0 +1,58 @@
+import json
+from fastapi import FastAPI
+import snscrape.modules.twitter as sntwitter
+import re    # RegEx for removing non-letter characters
+import pickle
+from keras.utils import pad_sequences
+from keras.models import load_model
+
+app = FastAPI()
+
+tokenizer=None
+with open('./models/tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+sentiment_model=load_model('./models/best_model.h5')
+sentiments=['Negative','Neutral','Positive']
+
+def tweet_to_words(tweet):
+    ''' Convert tweet text into a sequence of words '''
+    max_len=100
+    # convert to lowercase
+    text = tweet.lower()
+    text = re.sub(r"\S*https?:\S*", "", text)
+    # remove non letters
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+    text = re.sub(r"\s+[a-zA-Z]\s+", ' ', text)  # When we remove apostrophe from the word "Mark's", the apostrophe is replaced by an empty space. Hence, we are left with single character "s" that we are removing here.
+    # Remove multiple spaces
+    text = re.sub(r'\s+', ' ', text)  # Next, we remove all the single characters and replace it by a space which creates multiple spaces in our text. Finally, we remove the multiple spaces from our text as well.
+
+    text=[text]
+    text = tokenizer.texts_to_sequences(text)
+    text = pad_sequences(text, padding='post', maxlen=max_len)
+    result=sentiment_model.predict(text).argmax(1)
+    result=sentiments[result[0]]
+    return result
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/api/get_twitter_sentiments/{data}")
+def get_twitter_sentiments(data):
+    topic=data
+    hashtag=data
+    # query="pathaan (#pathaan) until:2023-01-14 since:2022-12-01"
+    # query = topic + " (#" + hashtag + ") " + "until:"+totimeyyyy+"-"+totimemm+"-"+totimeday+ " since:"+fromtimeyyyy+"-"+fromtimemm +"-"+fromtimeday +" lang:en"
+    query = topic +" lang:en"
+    tweets=[]
+    limits=100
+    for tweet in sntwitter.TwitterSearchScraper('(from:elonmusk) until:2022-01-01 since:2010-01-01').get_items():
+        if len(tweets)==limits:
+            break
+        else:
+            tweets.append(tweet.rawContent)
+    # tweets = list(map(tweet_to_words,tweets))
+    # print(tweets[0])
+    return {'list':"hii"}
